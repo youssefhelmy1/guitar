734d3b0275ef05e9ab108e4ca4cd43baba692786
youssefhelmy1 <yousefhelmy399@gmail.com>
2025-03-22 11:28:00 +0300

    new website

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById("signupForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Signup successful!");
+});
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Login successful!");
+});
