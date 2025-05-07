f67cffaa156f71d92ed1642beb204ad822ccb265
youssefhelmy1 <yousefhelmy399@gmail.com>
2025-05-07 07:06:04 +0300

    signup

--- users.sql
+++ users.sql
@@ -1,15 +1,17 @@
 -- Active: 1716015696405@@127.0.0.1@3306@y7elmy
- 
- CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL UNIQUE,
-  `password` varchar(255) NOT NULL,
-  `experience_level` enum('beginner','intermediate','advanced') NOT NULL,
-  `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-);
+ DROP TABLE IF EXISTS users;
+ ALTER TABLE users DISCARD TABLESPACE users;
 
+  CREATE TABLE `users` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) NOT NULL,
+    `email` varchar(100) NOT NULL UNIQUE,
+    `password` varchar(255) NOT NULL,
+    `experience_level` enum('beginner','intermediate','advanced') NOT NULL,
+    `created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+  );
+ DROP TABLE IF EXISTS bookings;
 CREATE TABLE `bookings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
